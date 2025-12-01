9c26d5e2f13e1475896e42a827ce3e623ae71b3a
LaCreArthur <le-tutur@hotmail.fr>
2025-12-01 18:29:15 +0100

    feat: add AndroidAutoKey for automatic keystore configuration and update TMP settings for emoji support

---
+++ Assets/Scripts/Editor/AndroidAutoKey.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+
+[InitializeOnLoad]
+public class AndroidAutoKey
+{
+    #if UNITY_EDITOR
+
+    static AndroidAutoKey()
+    {
+        #if UNITY_EDITOR_OSX
+        PlayerSettings.Android.keystoreName = "/Users/arthur/unity-projects/unity-fastlane-ci/user.keystore";
+        #endif
+        PlayerSettings.Android.keystorePass = "750015";
+        PlayerSettings.Android.keyaliasPass = "750015";
+    }
+
+    #endif
+}
